ff09275ea757901a70e82b7131bacf85ecae0820
Jonathan Reátegui <j.reategui2012@gmail.com>
2024-08-09 09:44:10 -0400

    Creating menu layout, adding to menu bar, and handling item selection

--- app/src/main/java/com/example/menuoptionsdemoapp/MainActivity.kt
+++ app/src/main/java/com/example/menuoptionsdemoapp/MainActivity.kt
@@ -3,9 +3,13 @@ package com.example.menuoptionsdemoapp
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.example.menuoptionsdemoapp.databinding.ActivityMainBinding
 import com.example.menuoptionsdemoapp.databinding.ActivityScreen2Binding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -24,6 +28,42 @@ class MainActivity : AppCompatActivity() {
             goToNextPage()
         }
     }
+
+    // The onCreateOptionsMenu function causes the Activity to display the options menu in the screen’s title bar
+    // However, it does NOT control what happens when you “click” on a menu option
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.settings_menu, menu)
+        return true
+    }
+
+    // The onOptionsItemSelected function is used to handle clicks on each item in the options menu
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection.
+        return when (item.itemId) {
+            R.id.mi_new_tab -> {
+                // do something
+                val snackBar = Snackbar.make(binding.root, "Opening a new tab...", Snackbar.LENGTH_LONG)
+                snackBar.show()
+                true
+            }
+            R.id.mi_new_incognito_tab -> {
+                // do something
+                goToNextPage()
+                true
+            }
+            R.id.mi_history -> {
+                // do something
+                true
+            }
+            R.id.mi_clear_browsing_data -> {
+                // do something
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 
     private fun goToNextPage() {
         val intent: Intent = Intent(this@MainActivity, Screen2::class.java)
