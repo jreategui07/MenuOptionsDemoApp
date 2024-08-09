7b11ff95ba819c87f7ae278916860f153e36dc1e
Jonathan Reátegui <j.reategui2012@gmail.com>
2024-08-09 09:28:59 -0400

    Setting title, navigating to screen 2 and enabling back button

--- app/src/main/java/com/example/menuoptionsdemoapp/MainActivity.kt
+++ app/src/main/java/com/example/menuoptionsdemoapp/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.menuoptionsdemoapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.menuoptionsdemoapp.databinding.ActivityMainBinding
+import com.example.menuoptionsdemoapp.databinding.ActivityScreen2Binding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -12,5 +15,18 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if (supportActionBar != null) {
+            supportActionBar!!.setTitle("Menu") // Setting the action bar title
+        }
+
+        binding.btnNext.setOnClickListener {
+            goToNextPage()
+        }
+    }
+
+    private fun goToNextPage() {
+        val intent: Intent = Intent(this@MainActivity, Screen2::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/menuoptionsdemoapp/Screen2.kt
@@ -0,0 +1,33 @@
+package com.example.menuoptionsdemoapp
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.example.menuoptionsdemoapp.databinding.ActivityScreen2Binding
+
+class Screen2 : AppCompatActivity() {
+    lateinit var binding: ActivityScreen2Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityScreen2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (supportActionBar != null) {
+            supportActionBar!!.setTitle("Screen 2")
+            supportActionBar!!.setDisplayHomeAsUpEnabled(true); // Enabling the back button
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle menu item selection
+        return when (item.itemId) {
+            android.R.id.home -> { // The name of the back button is: android.R.id.home
+                this.finish();
+                return true;
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
